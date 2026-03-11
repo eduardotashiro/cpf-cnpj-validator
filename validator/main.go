3cde3015eee8697c5d14a67e5b62abba9740b010
Eduardo Tashiro <eduardogaia854@hotmail.com>
2026-03-11 11:16:30 -0300

    primeiros passos...

---
+++ validator/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func validatorFistDigit(cpf int) bool  {
+	var sum int 
+	println(sum)
+
+	for i := 0; i < 9; i++ {
+		// result :=
+	}
+//    sum += result * (10 - i)
+	// (sum * 10) % 11
+	return false
+}
+
+func main()  {
+	fmt.Println("teste")
+	validatorFistDigit(52998224725)
+}
